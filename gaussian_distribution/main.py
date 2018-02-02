196739231b1b4a68cb6487b658f8011d30451ac6
carsonkk <carsonkk@gmail.com>
2018-02-01 17:14:05 -0800

    finish angles, start projection

--- gaussian_distribution/main.py
+++ gaussian_distribution/main.py
@@ -1,7 +1,6 @@
-import math
+import math as m
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
 import scipy.stats as stats
 
 
@@ -10,7 +9,7 @@ def gen_point(dim, mean, std):
   return np.random.normal(mean, std, dim)
 
 
-# Generate a certain number of points for the given dimeansion
+# Generate a specified number of points for the given dimension
 def gen_point_set(ct, dim, mean, std):
   point_set = []
 
@@ -20,35 +19,65 @@ def gen_point_set(ct, dim, mean, std):
   return point_set
 
 
+# Compute the distance between two points in d dimension using the Euclidian formula
+def dist_btwn_two_pts(x, y):
+  summation = 0.0
+
+  for i in xrange(len(x)):
+    summation += m.pow(x[i] - y[i], 2)
+
+  return m.sqrt(summation)
+
+
+# Compute the angle between two points in d dimension using a Law of Cosines formula
+def angle_btwn_two_pts(x, y):
+  origin = [0.0] * len(x)
+  xo_dist = dist_btwn_two_pts(x, origin)
+  yo_dist = dist_btwn_two_pts(y, origin)
+  xy_dist = dist_btwn_two_pts(x, y)
+
+  return m.degrees(m.acos((m.pow(xo_dist, 2) + m.pow(yo_dist, 2) - m.pow(xy_dist, 2)) / (2 * xo_dist * yo_dist)))
+
+
 # Compute the distance of each of the given points from the origin
 def dists_from_origin(pts):
   distances = []
+  origin = [0.0] * len(pts[0])
 
   for i in xrange(len(pts)):
-    summation = 0.0
-    for j in xrange(len(pts[i])):
-      summation += (pts[i][j] * pts[i][j])
-    distances.append(math.sqrt(summation))
+    distances.append(dist_btwn_two_pts(pts[i], origin))
 
   return distances
 
 
 # Compute the distance between each pair of points
-def dists_between_pts(pts):
-  distances = np.zeros((len(pts), len(pts)))
+def dists_btwn_pts(pts):
+  distances = []
+  distances_matrix = np.zeros((len(pts), len(pts)))
 
   for i in xrange(len(pts)):
     for j in xrange(i + 1, len(pts)):
-      summation = 0.0
-      for k in xrange(len(pts[i])):
-        summation += (pts[i][k] - pts[j][k]) * (pts[i][k] - pts[j][k])
-      distances[i][j] = math.sqrt(summation)
+      distances_matrix[i][j] = dist_btwn_two_pts(pts[i], pts[j])
+      distances.append(distances_matrix[i][j])
 
-  return distances
+  return distances, distances_matrix
+
+
+# Compute the angle between each pair of points, where each point forms a line with the origin and the angle is measured at the origin intersection
+def angles_btwn_pts(pts):
+  angles = []
+  angles_matrix = np.zeros((len(pts), len(pts)))
+
+  for i in xrange(len(pts)):
+    for j in xrange(i + 1, len(pts)):
+      angles_matrix[i][j] = angle_btwn_two_pts(pts[i], pts[j])
+      angles.append(angles_matrix[i][j])
+
+  return angles, angles_matrix
 
 
 # Plot the distribution of the given points
-def plot_distribution(pts, dim, ct, title, x_label, y_label):
+def plot_distribution(pts, dim, ct, title, x_label, y_label, fname):
   sorted_points = sorted(pts)
   mean = np.mean(sorted_points)
   deviation = np.std(sorted_points)
@@ -67,122 +96,26 @@ def plot_distribution(pts, dim, ct, title, x_label, y_label):
     bbox=dict(boxstyle="round", facecolor='#FFFFFF', ec="0.5", pad=0.5, alpha=1))
   plt.plot(sorted_points, fitted_curve, '-o')
   plt.hist(sorted_points, normed=True)
-  plt.savefig('distribution_from_origin.svg', bbox_inches='tight')
+  plt.savefig(fname, bbox_inches='tight')
   plt.show()
 
 
 # Main entry point
 if __name__ == '__main__':
-  dimension = 900
-  count = 2000
+  dimension = 1000
+  count = 200
 
   # Get the set of basis points
   points = gen_point_set(count, dimension, 0, 1)
 
   # Compute the distribution of distances from the origin for each point and plot it
-  distances_from_origin = dists_from_origin(points)
-  plot_distribution(distances_from_origin, dimension, count, "Distribution From Origin", "Value", "Frequency")
+  origin_distances = dists_from_origin(points)
+  plot_distribution(origin_distances, dimension, count, "Distribution From Origin", "Distance", "Frequency", "origin_distance_distribution.svg")
 
+  # Compute the distribution of the distances bettwen each pairwaise grouping of points
+  pairwise_distances, pairwise_distances_matrix = dists_btwn_pts(points)
+  plot_distribution(pairwise_distances, dimension, count, "Distribution Between Pairs", "Distance", "Frequency", "pairwise_distance_distribution.svg")
 
-
-
-
-# Generate a spherical Gaussain distribution
-#   d - The dimension of the space
-#   x - The number of points to generate
-#   m - The mean (default is 0)
-#   v - The variance (default is 1)
-# def gen_vectors_old(d, x, m, v):
-#   points = []
-#   calc_mean = np.zeros(d)
-#   calc_var = np.zeros(d)
-
-#   # Find the mean
-#   for i in range(x):
-#     points.append(np.random.multivariate_normal(m, v))
-#     calc_mean += (points[i] / x)
-
-#   # Find the variance
-#   for i in range(x):
-#     calc_var += (np.power((points[i] - calc_mean), 2) / x)
-
-#   # Find the standard deviation
-#   calc_sigma = np.sqrt(calc_var)
-
-#   z = np.linspace(calc_mean - 3*calc_sigma, calc_mean + 3*calc_sigma, x)
-#   plt.plot(z, mlab.normpdf(z, calc_mean, calc_sigma))
-
-#   plt.show()
-#   print("Generation Completed")
-#   return points, calc_mean, calc_var
-
-  #target_mean = 0
-  #target_variance = 1
-
-  #mean_arr = [target_mean] * dimension
-  #var_arr = [target_variance] * dimension
-  #cov_arr = [var_arr] * dimension
-
-  #points, calc_mean, calc_var = generator(dimension, sample, mean_arr, cov_arr)
-
-  ##vectors = gen_gaussian_vectors(dimension, count)
-  #vectors = sample_spherical(1, 50)
-  #printPoints(vectors)
-  ##vector_mean = calcVectorMean(vectors)
-  ##sorted_mean = sorted(vector_mean)
-  ##total_mean= np.mean(sorted_mean)
-  ##total_std = np.std(sorted_mean)
-  ##print total_mean
-  ##print total_std
-
-  ##fit = stats.norm.pdf(sorted_mean, total_mean, total_std)
-  ##plt.plot(sorted_mean, fit, '-o')
-  ##plt.hist(sorted_mean, normed=True)
-  ##plt.show()
-
-
-
-# # Generate a single random number according to a unit Gaussian distribution (0-mean, 1-var)
-# def gen_gaussian():
-#   basis = 0
-
-#   while basis == 0:
-#     basis = round(np.random.random() * 100)
-
-#   vals = np.random.random(int(basis))
-#   gaussian = (float(np.sum(vals)) - (basis / 2)) / math.sqrt(basis / 12.0)
-
-#   return gaussian
-
-
-# # Generate a set of Gaussian vector
-# def gen_gaussian_vectors(dimension, count):
-#   vecs = []
-
-#   for i in xrange(count):
-#     dim_vec = []
-#     for j in xrange(dimension):
-#       dim_vec.append(gen_gaussian())
-
-#     vecs.append(dim_vec)
-
-#   print("Generation Completed")
-#   return vecs
-
-
-# # Calculate the mean
-# def calcVectorMean(vectors):
-#   return np.divide(np.sum(vectors, axis=0), len(vectors))
-
-
-
-# # Print out the list of points
-# def printPoints(vectors):
-#   for i in range(len(vectors)):
-#     print("{}\n".format(vectors[i]))
-
-
-# def sample_spherical(npoints, ndim=3):
-#   vec = np.random.randn(ndim, npoints)
-#   vec /= np.linalg.norm(vec, axis=0)
-#   return vec
+  # Compute the distribution of the angles bettwen each pairwaise grouping of points
+  pairwise_angles, pairwise_angles_matrix = angles_btwn_pts(points)
+  plot_distribution(pairwise_angles, dimension, count, "Distribution Between Pairs", "Angle (degrees)", "Frequency", "pairwise_angle_distribution.svg")
