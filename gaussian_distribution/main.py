0de0f4f0d38f2910f6d935f608adfa6c9681c5c4
carsonkk <carsonkk@gmail.com>
2018-02-16 15:29:57 -0800

    finished projection tests

--- gaussian_distribution/main.py
+++ gaussian_distribution/main.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
   # Compute the distribution of distances from the origin for each point and plot it
   origin_distances = dists_from_origin(points)
   plot_distribution(origin_distances, dimension, count, "Distribution From Origin", "Distance", "Probability", 
-    "figs/origin_distance_distribution_d-" + str(dimension) + "_n-" + str(count) + ".svg")
+    "figs/origin_distance_distribution_d-" + str(dimension) + "_n-" + str(count) + ".png")
 
   # Compute the distribution of the distances bettwen each pairwaise grouping of points
   pairwise_distances, pairwise_distances_matrix = dists_btwn_pts(points)
   plot_distribution(pairwise_distances, dimension, count, "Distribution Between Pairs", "Distance", "Probability", 
-    "figs/pairwise_distance_distribution_d-" + str(dimension) + "_n-" + str(count) + ".svg")
+    "figs/pairwise_distance_distribution_d-" + str(dimension) + "_n-" + str(count) + ".png")
 
   # Compute the distribution of the angles bettwen each pairwaise grouping of points
   pairwise_angles, pairwise_angles_matrix = angles_btwn_pts(points)
   plot_distribution(pairwise_angles, dimension, count, "Distribution Between Pairs", "Angle (degrees)", 
-    "Probability", "figs/pairwise_angle_distribution_d-" + str(dimension) + "_n-" + str(count) + ".svg")
+    "Probability", "figs/pairwise_angle_distribution_d-" + str(dimension) + "_n-" + str(count) + ".png")
--- high_dimensional_projection/main.py
+++ high_dimensional_projection/main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
   # Compute the distribution of the distances between each pairwaise grouping of points in d-dim
   pairwise_distances_d, pairwise_distances_k_matrix = gdst.dists_btwn_pts(points)
   gdst.plot_distribution(pairwise_distances_d, dimension, count, "Distribution Between Pairs [d-dim]", "Distance", "Probability", 
-    "figs/pairwise_distance_distribution_d-dim_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".svg")
+    "figs/pairwise_distance_distribution_d-dim_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".png")
 
   # Project the set of basis points from d-dimensions to k-dimensions
   projection_points = project_point_set(points, dimension, subdimension, radius)
@@ -29,13 +29,13 @@ if __name__ == '__main__':
   # Compute the distribution of the distances between each pairwaise grouping of points in k-dim
   pairwise_distances_k, pairwise_distances_k_matrix = gdst.dists_btwn_pts(projection_points)
   gdst.plot_distribution(pairwise_distances_k, dimension, count, "Distribution Between Pairs [k-dim]", "Distance", "Probability", 
-    "figs/pairwise_distance_distribution_k-dim_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".svg", subdimension)
+    "figs/pairwise_distance_distribution_k-dim_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".png", subdimension)
 
   # Compute the difference between the original distance values * root(subdimension) and the distance values for the subdimension
   pairwaise_distances_d_scaled, avg_distance_d_scaled, avg_distance_k, max_distance_d_scaled, max_distance_k = projection_difference(
     pairwise_distances_d, pairwise_distances_k, dimension, subdimension, radius)
   gdst.plot_distribution(pairwaise_distances_d_scaled, dimension, count, "Distribution Between Pairs [d-dim scaled]", "Distance", "Probability", 
-    "figs/pairwise_distance_distribution_d-dim_scaled_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".svg", subdimension)
+    "figs/pairwise_distance_distribution_d-dim_scaled_d-" + str(dimension) + "_k-" + str(subdimension)+ "_n-" + str(count) + ".png", subdimension)
   print("Average Distance % Error")
   print("d-dim scaled: {}".format(avg_distance_d_scaled))
   print("k-dim: {}".format(avg_distance_k))
